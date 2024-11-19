696ff6b4244a0576422b62e62193802208a965fd
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2024-11-19 11:58:10 -0600

    dates work 9

--- actions/dates/dates.js
+++ actions/dates/dates.js
@@ -12,29 +12,22 @@ async function run() {
 
     const metaParsed = JSON.parse(exisitingMetadataJson); //Record<string string>
     const filesModifiedArr = filesModifiedInCommit.split("\n");
-    const allFilesInGitArr = filesModifiedInCommit.split("\n");
+    const allFilesInGitArr = allFilesInGit.split("\n");
     console.log(
       `Received ${allFilesInGit.length} all files and ${filesModifiedInCommit.length} modified files`
     );
+    const metaDataUpdated = allFilesInGitArr.reduce((acc, file) => {
+      const isModified = filesModifiedArr.includes(file);
+      const isInExistingMeta = Object.hasOwnProperty(metaParsed, file);
+      if (isModified || !isInExistingMeta) {
+        acc[file] = new Date().toISOString();
+      } else {
+        acc[file] = metaParsed[file];
+      }
+      return acc;
+    });
 
-    const metadataUpdated = Object.entries(metaParsed).reduce(
-      (acc, [key, value]) => {
-        if (!allFilesInGitArr.includes(key)) {
-          // If this key is in the metadata but not our project any more, don't return it;
-          return acc;
-        }
-        if (filesModifiedArr.includes(key)) {
-          //file has been updated;
-          acc[key] = new Date().toISOString();
-          return acc;
-        }
-        // file has not been updated
-        acc[key] = value;
-        return acc;
-      },
-      {}
-    );
-    const stringified = JSON.stringify(metadataUpdated);
+    const stringified = JSON.stringify(metaDataUpdated);
     if (stringified !== exisitingMetadataJson) {
       const existingMeta = await octokit.rest.repos.getContent({
         ...github.context.repo,
